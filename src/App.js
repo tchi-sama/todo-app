90f2cf4a828429dae2eb6eae5f278d2bfaf1ab03
tchi-sama <tchisama41@gmail.com>
2023-02-13 11:33:38 -0500

    create new , and delete , <3 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import DoneRoundedIcon from "@mui/icons-material/DoneRounded";
 import { useEffect, useState } from "react";
-import { collection, doc, onSnapshot, query, updateDoc } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, onSnapshot, query, updateDoc } from "firebase/firestore";
 import { db } from "./firebase";
 import Todo from "./Todo";
 function App() {
   const [todos, setTodos] = useState([]);
-
+  const [input, setInput] = useState("");
   //create todo 
+  const createTodo = async (e) => {
+    e.preventDefault();
+    if(input){
+      await addDoc(collection(db, "todos"), {
+        text:input,
+        complated:false,
+      })
+      setInput("");
+    }else{
+      alert("Please enter a todo");
+      return;
+    }
+  }
   //read todo
   useEffect(()=>{
     const q = query(collection(db,"todos"));
@@ -27,24 +40,26 @@ function App() {
     })
   };
   //delete todo
-
+  const deleteTodo = async (todo) => {
+    await deleteDoc(doc(db, "todos",todo.id))
+        };
   return (
     <div className="h-[100vh] bg-gray-100 py-12 flex items-center justify-center">
       <div className="w-[500px] h-full  flex flex-col gap-6">
         <div className="overflow-y-auto flex-1 flex flex-col gap-2 p-3 bg-white shadow-2xl rounded-xl border">
           {
             todos.map((todo) => (
-              <Todo toggleComplete={toggleComplete}  key={todo.id} todo={todo} />
+              <Todo deleteTodo={deleteTodo} toggleComplete={toggleComplete}  key={todo.id} todo={todo} />
             ))
           }
         </div>
-        <div className="border h-16 p-3 bg-white shadow-2xl rounded-xl flex justify-between gap-3">
-          <input type="text" className=" rounded w-full h-full outline-none" />
-          <button className="flex px-2 flex justify-center items-center gap-2 text-white bg-green-600 rounded-lg ">
+        <form onSubmit={createTodo} className="border h-16 p-3 bg-white shadow-2xl rounded-xl flex justify-between gap-3">
+          <input value={input} onChange={e=>setInput(e.target.value)} type="text" className=" rounded w-full h-full outline-none" />
+          <button className="flex px-2 justify-center items-center gap-2 text-white bg-green-600 rounded-lg ">
             add
             <DoneRoundedIcon />
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,10 +1,21 @@
-function Todo({todo , toggleComplete}) {
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import { IconButton } from "@mui/material";
+function Todo({ todo, toggleComplete,deleteTodo }) {
   return (
-    <div onClick={()=>toggleComplete(todo)} className="bg-gray-100 border rounded-lg p-2 gap-2 flex ">
-        <input checked={todo.complated} type="checkbox" name="" id="checkbox" />
-        <p htmlFor="checkbox" className="flex-1 p-2">{todo.text}</p>
+    <div
+      className="bg-gray-100 justify-center items-center border rounded-lg p-2 gap-2 flex "
+    >
+        <div className="flex flex-1" onClick={() => toggleComplete(todo)}>
+      <input checked={todo.complated} type="checkbox" name="" id="checkbox" />
+      <p  htmlFor="checkbox" className="flex-1 p-2">
+        {todo.text}
+      </p>
+        </div>
+      <IconButton onClick={()=>deleteTodo(todo)}>
+        <DeleteRoundedIcon />
+      </IconButton>
     </div>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
