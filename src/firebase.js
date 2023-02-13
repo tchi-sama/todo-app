11c372e1d038314d55af333daacb08f21fd4bfc7
tchi-sama <tchisama41@gmail.com>
2023-02-13 08:31:34 -0500

    setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCR3L7umRIA1gNons1FDQSf98h8Y9NyvME",
+  authDomain: "fir-crud-2e45a.firebaseapp.com",
+  projectId: "fir-crud-2e45a",
+  storageBucket: "fir-crud-2e45a.appspot.com",
+  messagingSenderId: "635571668434",
+  appId: "1:635571668434:web:3c2c5491f4f1ba6089f40b"
+};
+const app = initializeApp(firebaseConfig);
